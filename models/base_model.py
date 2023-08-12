4bf49de91135bd4f42e09e57f5a550bfdf1a78fd
Fikayomi31 <fikayoogundijo@gmail.com>
2023-08-12 18:49:45 +0000

    base_model

---
+++ models/base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Defining a basemodel class"""
+import uuid
+from _datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """Representation of class basemodel"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializing the class basemodel
+        Args:
+            *args: listnof argument
+            **kwarg: dict of key values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key  == 'created_at':
+                    self.__dict__['created_at'] = datetime.strptime(
+                            kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.__dict__['updated_at'] = datetime.strptime(
+                            kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Return string representation of the class"""
+        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Return updated updated_at"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return a dict representation of the class"""
+        my_dict = {}
+        my_dict.update(self.__dict__)
+        my_dict.update({"__class__":self.__class__.__name__})
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+
+        return my_dict
